4c851f88d26c75703ddcf716b4b6c867fb60abfb
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-12-22 01:58:09 +0100

    export helpers

--- src/PDDL.jl
+++ src/PDDL.jl
@@ -10,6 +10,9 @@ export write_domain, write_problem, write_pddl
 export load_domain, load_problem, preprocess
 export save_domain, save_problem
 export get_value
+export GoalSpec
+export has_term_in_state
+export flatten_goal
 export get_static_predicates, get_static_functions
 export satisfy, evaluate, find_matches
 export init_state, goal_state, initialize, transition, simulate
@@ -22,6 +25,7 @@ export clear_available_action_cache!, clear_relevant_action_cache!
 include("requirements.jl")
 include("structs.jl")
 include("parser.jl")
+include("goals.jl")
 include("writer.jl")
 
 using .Parser, .Writer
